67faacf6599ca0901212ccc1ac843bbc330cba12
janvierjr <bernie@myheartsleeve.com>
2023-12-11 13:47:12 -0600

    update button test

---
+++ index.js
@@ -0,0 +1,6 @@
+const addButton = document.getElementById('add-button');
+const input = document.getElementById('input-field');
+
+addButton.addEventListener('click', () => {
+  console.log('input', input.value)
+});
